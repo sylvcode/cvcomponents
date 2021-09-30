3e6bbbeafaa2efcc327bdee47f30c98b123bec91
Bobby Donchev <contact@donchev.is>
2021-09-30 09:20:46 +0200

    smaller column

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ export default function App() {
     <div className="App">
       <div className="container">
         <div className="row">
-          <div className="col-12 col-md-8">
+          <div className="col-12 col-md-6">
             <Career />
+            {}
             <Education />
           </div>
           <div className="col-6 col-md-4">
